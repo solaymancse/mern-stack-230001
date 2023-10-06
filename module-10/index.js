b9a9902af29e16d2db192d0bdfa492afde512fdf
wellupweb <wellupweb@gmail.com>
2023-10-06 17:33:46 +0600

    module 10

---
+++ module-10/index.js
@@ -0,0 +1,16 @@
+var a = 10;
+var b = 5;
+var c = a + b;
+
+console.log("additon c = ", c)
+
+
+// Home wrok
+
+// calculate two numbers to follow the below
+// 1. addition
+// 2. multiplication
+// 3. substraction
+// 4. division
+
+
