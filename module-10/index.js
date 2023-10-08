09ed577726abe8b27feba15bbbfa1a3e9cee3271
wellupweb <wellupweb@gmail.com>
2023-10-08 16:00:49 +0600

    js

--- module-10/index.js
+++ module-10/index.js
@@ -1,8 +1,15 @@
-var a = 10;
-var b = 5;
-var c = a + b;
+// var a = 1;
+// var b = 5;
+// var c = a + b;
 
-console.log("additon c = ", c)
+// console.log("additon c = ", c)
+
+// // divison
+// var a = 1;
+// var b = 5;
+// var c = b/a;
+
+// console.log("divison c = ", c)
 
 
 // Home wrok
@@ -12,5 +19,45 @@ console.log("additon c = ", c)
 // 2. multiplication
 // 3. substraction
 // 4. division
+
+
+// 8/10/23
+var name = "My Name is Solayman"
+var age = 26
+console.log(typeof name);
+console.log(typeof age);
+
+console.log(name.toLowerCase())
+console.log(name.toUpperCase())
+console.log(name.length)
+console.log(age.length)
+console.log(name.split('am'))
+
+function addTwoNumber(a,b){
+    var a;
+    var b;
+    var c = a +b;
+
+    return c;
+}
+
+console.log(addTwoNumber(10,12))
+
+var a = 1.7;
+console.log(Math.round(a))
+
+
+// condition
+
+var marks = 70;
+
+if(marks >= 80){
+console.log("A+")
+}else if(marks >=60){
+    console.log("B")
+
+}else{
+    console.log("fail.")
+}
 
 
